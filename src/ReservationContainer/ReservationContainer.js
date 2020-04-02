12baa27c5431afdcd0141318edea4ec8813b655a
Trond Makonese <trondation@gmail.com>
2020-04-02 10:12:27 -0600

    All reservations now show up on the dom.

--- src/ReservationContainer/ReservationContainer.js
+++ src/ReservationContainer/ReservationContainer.js
@@ -3,9 +3,21 @@ import { Reservations } from '../Reservations/Reservations'
 
 export const ReservationsContainer = (props) => {
   console.log(props.reservations)
+  const allReservations = props.reservations.map(res => {
+    console.log(res)
+    return <Reservations
+      name={res.name}
+      date={res.date}
+      time={res.time}
+      number={res.number}
+      id={res.id}
+      key={res.id}
+      />
+  })
 
   return (
     <section>
+      {allReservations}
     </section>
   )
 }
