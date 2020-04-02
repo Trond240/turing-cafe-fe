17874dab95d8de53b3dbde9c693c3ea1bed3c1f5
Trond Makonese <trondation@gmail.com>
2020-04-02 10:45:31 -0600

    build out add reservation form.

--- src/addReservation/addReservation.js
+++ src/addReservation/addReservation.js
@@ -8,13 +8,42 @@ class AddReservation extends Componet {
       date: '',
       id: '',
       name: '',
+      number: '',
       time: ''
     }
   }
 
   render() {
     return(
-      
+      <form>
+        <input
+        type='text'
+        placeholder='name'
+        name='name'
+        onChnage={}
+        />
+        <input
+        type='text'
+        placeholder='name'
+        name='name'
+        onChnage={}
+        />
+        <input
+        type='time'
+        placeholder='choose a time'
+        name='time'
+        onChnage={}
+        />
+        <input
+        type='number'
+        placeholder='number of guest'
+        name='number'
+        onChnage={}
+        />
+        <button>Book Reservation</button>
+      </form>
     )
   }
 }
+
+export default AddReservation;
