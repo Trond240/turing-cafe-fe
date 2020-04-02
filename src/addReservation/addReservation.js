9ec92ecbbf50380cdee10ce3f5acb829031dbe3d
Trond Makonese <trondation@gmail.com>
2020-04-02 10:37:49 -0600

    Create skeleton for addReservation form.

---
+++ src/addReservation/addReservation.js
@@ -0,0 +1,20 @@
+import React, { Component } form 'react';
+
+
+class AddReservation extends Componet {
+  constructor() {
+    suoer();
+    this.state = {
+      date: '',
+      id: '',
+      name: '',
+      time: ''
+    }
+  }
+
+  render() {
+    return(
+      
+    )
+  }
+}
